4087a6281ea8668a0690e54062b4dcf18e93bfc2
Colm Ahern <colm.ahern@gmail.com>
2025-02-22 20:33:36 +0000

    CreateArticleHandler called by UI

---
+++ BlazingBlog.WebUI.Server/Components/Pages/ArticleModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazingBlog.WebUI.Server.Components.Pages
+{
+    public class ArticleModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Content { get; set; }
+        public DateTime DatePublished { get; set; } = DateTime.Now;
+        public bool IsPublished { get; set; } = false;
+
+    }
+}
