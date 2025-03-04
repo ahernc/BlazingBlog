98b969703d71424c46741e061b77d324525ed9dc
Colm Ahern <colm.ahern@gmail.com>
2025-03-04 18:14:14 +0000

    Basic Login working

---
+++ BlazingBlog.WebUI.Server/Features/Users/LoginUserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazingBlog.WebUI.Server.Features.Users
+{
+    public sealed class LoginUserModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        [Display(Name = "User name")]
+        public string UserName { get; set; } = "";
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; } = "";
+    }
+}
