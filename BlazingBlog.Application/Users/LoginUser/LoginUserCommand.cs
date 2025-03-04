865a2b69fc50e44c047285cba98cf15b559da8db
Colm Ahern <colm.ahern@gmail.com>
2025-03-04 15:34:33 +0000

    LoginUserCommand and RegisterUserCommand and handlers added

---
+++ BlazingBlog.Application/Users/LoginUser/LoginUserCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazingBlog.Application.Users.LoginUser
+{
+    public class LoginUserCommand : ICommand
+    {
+        public required string UserName { get; set; }
+        public required string Password { get; set; }
+    }
+}
---
+++ BlazingBlog.Application/Users/LoginUser/LoginUserCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using BlazingBlog.Application.Authentication;
+
+namespace BlazingBlog.Application.Users.LoginUser
+{
+    internal class LoginUserCommandHandler : ICommandHandler<LoginUserCommand>
+    {
+
+        private readonly IAuthenticationService _authenticationService;
+
+        public LoginUserCommandHandler(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        public async Task<Result> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        {
+            var success = await _authenticationService.LoginUserAsync
+                (
+                    request.UserName,
+                    request.Password
+                );
+
+            if (success)
+            {
+                return Result.Ok();
+            }
+
+            return Result.Fail("Username or password is incorrect");
+        }
+    }
+}
