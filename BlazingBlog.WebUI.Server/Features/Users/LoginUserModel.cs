405e527e56c2142685ce7de18c179fe31365688b
Colm Ahern <colm.ahern@gmail.com>
2025-03-05 09:24:31 +0000

    Some small tidy up

--- BlazingBlog.WebUI.Server/Features/Users/LoginUserModel.cs
+++ BlazingBlog.WebUI.Server/Features/Users/LoginUserModel.cs
@@ -6,11 +6,11 @@ namespace BlazingBlog.WebUI.Server.Features.Users
     {
         [Required]
         [StringLength(100, MinimumLength = 3)]
-        [Display(Name = "User name")]
+        [Display(Name = "Username")]
         public string UserName { get; set; } = "";
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [StringLength(100)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; } = "";
