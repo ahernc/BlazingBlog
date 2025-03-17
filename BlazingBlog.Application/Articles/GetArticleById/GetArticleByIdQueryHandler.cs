8d741b33738fddecda08ee01fb69079143478760
Colm Ahern <colm.ahern@gmail.com>
2025-03-17 18:55:15 +0000

    UserService used everywhere we can use it now

--- BlazingBlog.Application/Articles/GetArticleById/GetArticleByIdQueryHandler.cs
+++ BlazingBlog.Application/Articles/GetArticleById/GetArticleByIdQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using BlazingBlog.Domain.Articles;
+﻿using BlazingBlog.Application.Users;
+using BlazingBlog.Domain.Articles;
 using BlazingBlog.Domain.Users;
 
 namespace BlazingBlog.Application.Articles.GetArticleById
@@ -7,34 +8,38 @@ namespace BlazingBlog.Application.Articles.GetArticleById
     {
         private readonly IArticleRepository _articleRepository;
         private readonly IUserRepository _userRepository;
-        public GetArticleByIdQueryHandler(IArticleRepository articleRepository, IUserRepository userRepository)
+        private readonly IUserService _userService;
+        public GetArticleByIdQueryHandler(IArticleRepository articleRepository, IUserRepository userRepository, IUserService userService)
         {
             _articleRepository = articleRepository;
             _userRepository = userRepository;
+            _userService = userService;
         }
 
         public async Task<Result<ArticleResponse?>> Handle(GetArticleByIdQuery request, CancellationToken cancellationToken)
         {
-            var result = await _articleRepository.GetArticleByIdAsync(request.Id);
-            var response = new ArticleResponse();
+            var article = await _articleRepository.GetArticleByIdAsync(request.Id);
+            var articleResponse = new ArticleResponse();
 
-            if (result is null)
+            if (article is null)
             {
                 return Result.Fail<ArticleResponse?>("The article does not exist.");
             }
             else
             {
-                response = result.Adapt<ArticleResponse>();
-                if (result.UserId is not null)
+                articleResponse = article.Adapt<ArticleResponse>();
+                if (article.UserId is not null)
                 {
-                    var author = await _userRepository.GetUserByIdAsync(result.UserId);
-                    response.UserName = author?.UserName ?? "Unknown";
+                    var author = await _userRepository.GetUserByIdAsync(article.UserId);
+                    articleResponse.UserName = author?.UserName ?? "Unknown";
+                    articleResponse.UserId = article.UserId;
+                    articleResponse.CanEdit = await _userService.CurrentUserCanEditArticleAsync(article.Id);
                 }
                 else
                 {
-                    response.UserName = "Unknown";
+                    articleResponse.UserName = "Unknown";
                 }
-                return response;
+                return articleResponse;
             }
         }
     }
---
+++ BlazingBlog.Application/Articles/GetArticleByIdForEditing/GetArticleByIdForEditingQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using BlazingBlog.Application.Users;
+using BlazingBlog.Domain.Articles;
+
+namespace BlazingBlog.Application.Articles.GetArticleByIdForEditing
+{
+    public class GetArticleByIdForEditingQueryHandler : IQueryHandler<GetArticleByIdForEditingQuery, ArticleResponse?>
+    {
+        private readonly IArticleRepository _articleRepository;
+        private readonly IUserService _userService;
+
+        public GetArticleByIdForEditingQueryHandler(IUserService userService, IArticleRepository articleRepository)
+        {
+            _userService = userService;
+            _articleRepository = articleRepository;
+        }
+
+        public async Task<Result<ArticleResponse?>> Handle(GetArticleByIdForEditingQuery request, CancellationToken cancellationToken)
+        {
+            var canEdit = await _userService.CurrentUserCanEditArticleAsync(request.Id);
+            if (!canEdit)
+            {
+                return Result.Fail<ArticleResponse?>("You are not allowed to edit this Article!");
+            }
+
+            var result = await _articleRepository.GetArticleByIdAsync(request.Id);
+            var articleResponse = result.Adapt<ArticleResponse>();
+            return articleResponse;
+        }
+    }
+}
