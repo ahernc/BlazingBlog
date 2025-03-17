9a719007c6a1bb5d7e58a255329f0f4e375468ee
Colm Ahern <colm.ahern@gmail.com>
2025-03-17 14:16:57 +0000

    Create Article fixed - now using UserService

--- BlazingBlog.Application/Articles/CreateArticle/CreateArticleCommandHandler.cs
+++ BlazingBlog.Application/Articles/CreateArticle/CreateArticleCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿using BlazingBlog.Domain.Articles;
+﻿using BlazingBlog.Application.Exceptions;
+using BlazingBlog.Application.Users;
+using BlazingBlog.Domain.Articles;
 
 namespace BlazingBlog.Application.Articles.CreateArticle
 {
@@ -6,19 +8,44 @@ namespace BlazingBlog.Application.Articles.CreateArticle
     {
 
         private readonly IArticleRepository _articleRepository;
+        private readonly IUserService _userService;
 
-        public CreateArticleCommandHandler(IArticleRepository articleRepository)
+        public CreateArticleCommandHandler(IArticleRepository articleRepository,
+            IUserService userService)
         {
             _articleRepository = articleRepository;
+            _userService = userService;
         }
 
         public async Task<Result<ArticleResponse>> Handle(CreateArticleCommand request, CancellationToken cancellationToken)
         {
-            var newArticle = request.Adapt<Article>();
-            var article = await _articleRepository.CreateArticleAsync(newArticle);
-            return article.Adapt<ArticleResponse>();
+            try
+            {
+                var newArticle = request.Adapt<Article>();
+
+                newArticle.UserId = await _userService.GetCurrentUserIdAsync();
+                if (await _userService.CurrentUserCanCreateArticleAsync() == false)
+                {
+                    return FailingResult();
+                }
+
+                var article = await _articleRepository.CreateArticleAsync(newArticle);
+
+                return article.Adapt<ArticleResponse>();
+            }
+            catch (UserNotAuthorizedException)
+            {
+                return FailingResult();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Some strange error occurred while trying to create an article. {ex.Message}");
+            }
         }
 
-
+        private Result<ArticleResponse> FailingResult()
+        {
+            return Result.Fail<ArticleResponse>("You are not allowed to create articles. Sorry!");
+        }
     }
 }
