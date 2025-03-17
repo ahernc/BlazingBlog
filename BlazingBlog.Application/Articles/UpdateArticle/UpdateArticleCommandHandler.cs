f27151811625c519ee7e96c5582aa89339e85247
Colm Ahern <colm.ahern@gmail.com>
2025-03-17 17:40:30 +0000

    Update Article now using User Services

--- BlazingBlog.Application/Articles/UpdateArticle/UpdateArticleCommandHandler.cs
+++ BlazingBlog.Application/Articles/UpdateArticle/UpdateArticleCommandHandler.cs
@@ -1,19 +1,28 @@
-﻿using BlazingBlog.Domain.Articles;
+﻿using BlazingBlog.Application.Users;
+using BlazingBlog.Domain.Articles;
 
 namespace BlazingBlog.Application.Articles.UpdateArticle
 {
     public class UpdateArticleCommandHandler : ICommandHandler<UpdateArticleCommand, ArticleResponse?>
     {
         private readonly IArticleRepository _articleRepository;
+        private readonly IUserService _userService;
 
-        public UpdateArticleCommandHandler(IArticleRepository articleRepository)
+        public UpdateArticleCommandHandler(IArticleRepository articleRepository,
+            IUserService userService)
         {
             _articleRepository = articleRepository;
+            _userService = userService;
         }
 
         public async Task<Result<ArticleResponse?>> Handle(UpdateArticleCommand request, CancellationToken cancellationToken)
         {
             var updatedArticle = request.Adapt<Article>();
+            if (!await _userService.CurrentUserCanEditArticleAsync(updatedArticle.Id))
+            {
+                return Result.Fail<ArticleResponse?>("You are not allowed to Edit this article! How did you get here?");
+            }
+
             var article = await _articleRepository.UpdateArticleAsync(updatedArticle);
             if (article is null)
             {
