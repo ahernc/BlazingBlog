6ca09f2267026775a97c71bc7075e0943eb69fa4
Colm Ahern <colm.ahern@gmail.com>
2025-03-19 23:12:47 +0000

    Basic Grid Added showing users

---
+++ BlazingBlog.Application/Users/GetUsers/GetUsersQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using BlazingBlog.Domain.Users;
+
+namespace BlazingBlog.Application.Users.GetUsers
+{
+    public class GetUsersQueryHandler : IQueryHandler<GetUsersQuery, List<UserResponse>>
+    {
+
+        private readonly IUserService _userService;
+        private readonly IUserRepository _userRepository;
+
+        public GetUsersQueryHandler(IUserService userService, IUserRepository userRepository)
+        {
+            _userService = userService;
+            _userRepository = userRepository;
+        }
+
+        public async Task<Result<List<UserResponse>>> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+        {
+            if (!await _userService.IsCurrentUserInRoleAsync("Admin"))
+            {
+                return Result.Fail<List<UserResponse>>("You are not allowed to see all users");
+            }
+            var users = await _userRepository.GetAllUsersAsync();
+            var response = users.Adapt<List<UserResponse>>();
+            return response;
+        }
+    }
+}
--- BlazingBlog.WebUI.Server/Program.cs
+++ BlazingBlog.WebUI.Server/Program.cs
@@ -5,7 +5,8 @@ using BlazingBlog.WebUI.Server;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents();
+builder.Services.AddRazorComponents() // static server side stuff...
+    .AddInteractiveServerComponents(); // server side stuf...
 
 // All depenencies in the ApplicationLayer are loaded here:
 builder.Services.AddApplication();
@@ -26,6 +27,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode(); // Configure to allow server render mode... 
 
 app.Run();
